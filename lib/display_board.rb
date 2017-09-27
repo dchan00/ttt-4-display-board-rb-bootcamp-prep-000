26e5ce4460d17edc9c53d68b3afa1927a6e2ef4e
DANIEL CHAN <dchan513@msn.com>
2017-09-27 02:49:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(arr)
+def display_board(arr = "   ")
+  
   puts "#{arr[0]}|#{arr[1]}|#{arr[2]}"
   puts "-----------"
   puts "#{arr[3]}|#{arr[4]}|#{arr[5]}"
